e3527247f69d7e122d080f9ee723837d87f1917c
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-06-26 23:40:13 -0300

    :tada: First commit

---
+++ src/main/java/github/matheusferreiral/fdeliveryapi/FoodDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package github.matheusferreiral.fdeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/matheusferreiral/fdeliveryapi/FoodDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.matheusferreiral.fdeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
